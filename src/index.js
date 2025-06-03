9f3cd0b9beb2310b989ed29ec8ee2f01ea8c92f7
sammri <ivildemort4837@gmail.com>
2025-06-03 18:46:19 +0300

    added styles

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { rps } from './js/rps.js';
+import { calculator } from './js/calculator.js';
+
+document.querySelector('#rps-root').innerHTML = rps;
+document.querySelector('#calc-root').innerHTML = calculator;
