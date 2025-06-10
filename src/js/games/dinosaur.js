29fee8185259258660ca03b369fc3cb6318c89d5
ulianaboiko <157517980+ulianaboiko@users.noreply.github.com>
2025-06-10 21:06:38 +0300

    started dinosaur

---
+++ src/js/games/dinosaur.js
@@ -0,0 +1,8 @@
+export const DinosaurrInit = () => {
+  const gameContainer = document.querySelector('.dinosaur');
+
+  const gameHtml = `<div class=""><h2 class=""></h2></div>
+    `;
+
+  gameContainer.innerHTML = gameHtml;
+};
