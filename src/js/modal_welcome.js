76527110bfc26ad5681ce6d2586db572078a518b
sheremetkyryl <kruisher@gmail.com>
2025-06-22 21:14:39 +0300

    доделал модалку в хедере

--- src/js/modal_welcome.js
+++ src/js/modal_welcome.js
@@ -1,9 +1,23 @@
-
+import sword from '../images/modal-windows/icon-sword.svg.svg'
+import puzzle from '../images/modal-windows/icon-puzzle.svg.svg'
+import devgame from '../images/modal-windows/icon-devgame.svg.svg'
+import tictactoe from '../images/modal-windows/icon-tictactoe.svg.svg'
   const container = document.querySelector(".modal-container");
 
   const modalHTML = `
     <div class="modal__overlay">
       <div class="modal">
+      <div class="modal__icons modal__icons--left">
+      
+      <div class="modal__icon__container--lefttop">
+          <img src="${sword}" alt="Піпетка" class="modal__icon top">
+      </div>
+
+         <div class="modal__icon__container--leftbotton">
+           <img src="${puzzle}" alt="Пазл" class="modal__icon bottom">
+          </div>
+
+       </div>
       <div class="modal__container">
         <button class="close-btn">&times;</button>
         <p class="modal__text">Привіт!<br>
@@ -17,6 +31,19 @@
         
         <button class="modal__button" id="saveBtn">Зберегти</button>
         </div>
+
+        <div class="modal__icons modal__icons--right">
+
+        <div class="modal__icon__container--righttop">
+         <img src="${devgame}" alt="Геймпад" class="modal__icon top">
+        </div>
+
+        <div class="modal__icon__container--rightbotton">
+         <img src="${tictactoe}" alt="Кубик" class="modal__icon bottom">
+        </div>
+
+       </div>
+
       </div>
     </div>`;
 
@@ -40,3 +67,21 @@
       alert("Будь ласка, введіть ім’я");
     }
   });
+
+
+const userNameHeader = document.querySelector(".header__text-user");
+function updateUsername() {
+  const storedName = localStorage.getItem("username");
+  if (storedName) {
+    userNameHeader.textContent = storedName;
+  }
+}
+saveBtn.addEventListener("click", () => {
+  const name = input.value;
+  if (name !== '') {
+    localStorage.setItem('username', name);
+    updateUsername();
+  } else {
+    alert("Будь ласка, введіть ім’я");
+  }
+});
