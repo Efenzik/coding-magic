0bf23f0a7d5f24e51b9f45143af578c45d7b4772
ulianaboiko <157517980+ulianaboiko@users.noreply.github.com>
2025-06-23 16:34:01 +0300

    виправила сам перемикач

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -1,7 +1,7 @@
 const themeChanger = document.querySelector('[data-theme-change]');
 
 const handleTheme = event => {
-  if (event.target.cheked) {
+  if (event.target.checked) {
     document.documentElement.dataset.theme = 'dark';
   } else {
     document.documentElement.dataset.theme = 'light';
