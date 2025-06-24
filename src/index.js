c503d2d8f1bb236ce6e7975e7abf97979686034c
Artem Cherkashin <157517719+Efenzik@users.noreply.github.com>
2025-06-24 20:55:18 +0300

    header-ended

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-import './js/modal_welcome.js'
+import './js/modal_welcome.js';
 import './js/modal_footer.js';
 import './js/main-content';
-import './js/main-content';
 import './js/filter-games';
-
